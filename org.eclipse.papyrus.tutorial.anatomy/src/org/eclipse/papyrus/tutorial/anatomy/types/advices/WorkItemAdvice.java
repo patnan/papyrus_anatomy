e73ee6312ed6ef505e3c92252ae024df4ea1ff87
Benoit maggi <benoit.maggi@cea.fr>
2016-05-31 22:13:25 +0200

    Create a WorkItem

--- org.eclipse.papyrus.tutorial.anatomy/src/org/eclipse/papyrus/tutorial/anatomy/types/advices/WorkItemAdvice.java
+++ org.eclipse.papyrus.tutorial.anatomy/src/org/eclipse/papyrus/tutorial/anatomy/types/advices/WorkItemAdvice.java
@@ -8,12 +8,12 @@ import org.eclipse.gmf.runtime.emf.type.core.commands.ConfigureElementCommand;
 import org.eclipse.gmf.runtime.emf.type.core.edithelper.AbstractEditHelperAdvice;
 import org.eclipse.gmf.runtime.emf.type.core.requests.ConfigureRequest;
 import org.eclipse.uml2.uml.Element;
-import org.eclipse.uml2.uml.Stereotype;
+import org.eclipse.uml2.uml.util.UMLUtil;
+
+import Anatomy.Type;
+import Anatomy.WorkPart;
 
 public class WorkItemAdvice extends AbstractEditHelperAdvice {
-	private static final String STEREOTYPE_PROPERTY__TYPE_VALUE = "WorkItem";
-	private static final String STEREOTYPE_PROPERTY__TYPE = "type";
-	private static final String ANATOMY_WORK_PART = "Anatomy::WorkPart";
 
 	@Override
 	protected org.eclipse.gmf.runtime.common.core.command.ICommand getAfterConfigureCommand(final ConfigureRequest request) {
@@ -21,15 +21,11 @@ public class WorkItemAdvice extends AbstractEditHelperAdvice {
 		return new ConfigureElementCommand(request) {
 			@Override
 			protected CommandResult doExecuteWithResult(IProgressMonitor progressMonitor, IAdaptable info) throws ExecutionException {
-				if (clazz.getAppliedStereotype(ANATOMY_WORK_PART) == null) {
-					Stereotype stereotype = clazz.getApplicableStereotype(ANATOMY_WORK_PART);
-					if (stereotype != null) {
-						clazz.applyStereotype(stereotype);
-						clazz.setValue(stereotype, STEREOTYPE_PROPERTY__TYPE, STEREOTYPE_PROPERTY__TYPE_VALUE);			    	  
-					}
-				}
+				 WorkPart stereotypeApplication = UMLUtil.getStereotypeApplication(clazz, WorkPart.class);
+				 stereotypeApplication.setType(Type.WORK_ITEM);
 				return CommandResult.newOKCommandResult(clazz);
 			}
 		};
 	}
+	
 }
